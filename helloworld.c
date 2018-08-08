2d6797d9635583bfe15449499cee2dcadb06a516
ghostwu <359173352@qq.com>
2018-08-08 14:05:08 +0800

    modify type of variable c

--- helloworld.c
+++ helloworld.c
@@ -5,7 +5,9 @@ int main(  void ) {
 	int a, b;
 	printf( "pls input a and b\n" );
 	scanf( "%d %d", &a, &b );
-	int c = a + b;
-	printf( "sum of a and b = %d\n", sum );
+	//int c = a + b;
+	double c = a + b;
+	//printf( "sum of a and b = %d\n", sum );
+	printf( "sum of a and b = %f\n", sum );
 	return 0;
 }
